1624b62438fa39f544e419058408fe519753eec3
Volker von Hoeßlin <skydiablo@gmx.net>
2021-08-27 16:25:01 +0200

    update deprecated const references

--- SkyDiabloDoctrineBundle.php
+++ SkyDiabloDoctrineBundle.php
@@ -3,6 +3,7 @@
 namespace SkyDiablo\DoctrineBundle;
 
 use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use SkyDiablo\DoctrineBundle\Bundle\RegisterDBALTypeBundle;
 use SkyDiablo\DoctrineBundle\DBAL\Types\Common\UTCDateTimeType;
 
@@ -10,8 +11,8 @@ class SkyDiabloDoctrineBundle extends RegisterDBALTypeBundle
 {
     protected function registerDBALTypes()
     {
-        Type::overrideType(Type::DATETIME, UTCDateTimeType::class); // all datetime objects will be stored as UTC
-        Type::overrideType(Type::DATETIMETZ, UTCDateTimeType::class); // all datetime objects will be stored as UTC
+        Type::overrideType(Types::DATETIME_MUTABLE, UTCDateTimeType::class); // all datetime objects will be stored as UTC
+        Type::overrideType(Types::DATETIMETZ_MUTABLE, UTCDateTimeType::class); // all datetime objects will be stored as UTC
     }
 
 }
